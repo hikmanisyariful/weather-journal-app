2e3c8ad5d10b89b3172bb74db08b2251c8687de6
hikmanisyariful <syarifulfajar017@gmail.com>
2021-11-21 16:36:09 +0700

    Fix style

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // Setup empty JS object to act as endpoint for all routes
-let projectData = {};
+const projectData = {};
 
 // Require Express to run server and routes
 const express = require("express");
